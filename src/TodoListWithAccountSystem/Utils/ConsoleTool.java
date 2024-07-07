1b4b0926eff5c548b67eaafc8cb0c0e9e7c48dda
gwongibeom <k201gun@kakao.com>
2024-07-07 21:55:42 +0900

    (개발) 콘솔 클리어 기능 개발
    - 콘솔 클래스 생성
    - 클리어 메소드 생성

---
+++ src/TodoListWithAccountSystem/Utils/ConsoleTool.java
@@ -0,0 +1,9 @@
+package TodoListWithAccountSystem.Utils;
+
+public class ConsoleTool {
+    public void clear() {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println();
+        }
+    }
+}

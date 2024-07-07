5bb4c9a1ae53ad90950c71aabcddee78581bff84
gwongibeom <k201gun@kakao.com>
2024-07-07 23:34:34 +0900

    (개발) 테스트 실행 오늘 개발 끝!

--- src/TodoListWithAccountSystem/Main.java
+++ src/TodoListWithAccountSystem/Main.java
@@ -1,7 +1,34 @@
 package TodoListWithAccountSystem;
 
+import TodoListWithAccountSystem.Account.AccountSystem;
+import TodoListWithAccountSystem.Todo.Todo;
+import TodoListWithAccountSystem.Todo.TodoListSystem;
+import TodoListWithAccountSystem.Utils.ConsoleTool;
+import TodoListWithAccountSystem.Utils.InputHandler;
+
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        AccountSystem accountSystem = new AccountSystem();
+        InputHandler inputHandler = new InputHandler();
+        TodoListSystem todoListSystem = new TodoListSystem();
+
+        String id = inputHandler.getStringInput("id: ");
+        String password = inputHandler.getStringInput("password: ");
+
+        accountSystem.addAccount(id, password);
+        accountSystem.login(id,password);
+
+        todoListSystem.addTodo("김치 썰기", accountSystem.getUserAccount().getAccountId());
+        ArrayList<Todo> test = todoListSystem.loadTodosByAccount(accountSystem.getUserAccount().getAccountId());
+
+        for (Todo todo : test) {
+            System.out.println(todo.getTitle());
+        }
+
+
+
     }
 }

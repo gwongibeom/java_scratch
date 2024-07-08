a4645a0d2120379dda1617f0b6c20c635cf4feb7
gwongibeom <k201gun@kakao.com>
2024-07-09 00:19:56 +0900

    (개발) InputHandler getIntInput 메소드 추가

--- src/TodoListWithAccountSystem/Utils/InputHandler.java
+++ src/TodoListWithAccountSystem/Utils/InputHandler.java
@@ -3,11 +3,23 @@ package TodoListWithAccountSystem.Utils;
 import java.util.Scanner;
 
 public class InputHandler {
-    public String getStringInput(String prompt){
+    public String getStringInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(prompt);
         String input = scanner.nextLine();
 
         return input.trim();
+    }
+
+    public int getIntInput(String prompt) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(prompt);
+            String input = scanner.nextLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("정수를 입력 해주세요.");
+            return 9999;
+        }
     }
 }

fb9a6bf774e320a54f4e225e8c1e21b0865c31b6
gwongibeom <k201gun@kakao.com>
2024-07-09 00:19:25 +0900

    (개발) ConsoleTool 딜레이 추가

--- src/TodoListWithAccountSystem/Utils/ConsoleTool.java
+++ src/TodoListWithAccountSystem/Utils/ConsoleTool.java
@@ -1,7 +1,8 @@
 package TodoListWithAccountSystem.Utils;
 
 public class ConsoleTool {
-    public void clear() {
+    public void clear() throws InterruptedException {
+        Thread.sleep(600);
         for (int i = 0; i <= 100; i++) {
             System.out.println();
         }

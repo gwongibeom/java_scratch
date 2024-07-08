37963cb7831859e1959592fba790ed42232de03c
gwongibeom <k201gun@kakao.com>
2024-07-08 13:59:54 +0900

    (개발) loadTodosByAccount => getTodosByAccount로 변경, printTodos 메소드 추가, 문서반영

--- src/TodoListWithAccountSystem/Todo/TodoListSystem.java
+++ src/TodoListWithAccountSystem/Todo/TodoListSystem.java
@@ -4,21 +4,25 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class TodoListSystem {
-    ArrayList<Todo> todoList = new ArrayList<Todo>();
+    private ArrayList<Todo> todoList = new ArrayList<Todo>();
 
     public void addTodo(String title, String accountId) {
         Todo newTodo = new Todo(todoList.size() + 1, title, accountId);
         todoList.add(newTodo);
     }
 
-    public ArrayList<Todo> loadTodosByAccount(String accountId) {
+    public ArrayList<Todo> getTodosByAccount(String accountId) {
         return todoList.stream()
                 .filter(todo -> todo.getAccountId().equals(accountId))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
+    public void printTodos(String accountId) {
+        getTodosByAccount(accountId).forEach(todo -> System.out.println(todo.getId() + " " + todo.getTitle() + " " + todo.getAccountId()));
+    }
+
     public void deleteTodo(int id) {
-        todoList.remove(id);
+        todoList.removeIf(todo -> todo.getId() == id);
     }
 
 
